6588e97a0db1cd19fb565186ab04ec1af87ba7b8
Biró László <birolaszlo@hotmail.com>
2024-12-11 19:09:58 +0100

    Day 04 Part 2 works

---
+++ 2024/Day 04/Part2.cs
@@ -0,0 +1,49 @@
+﻿namespace Day_04;
+
+internal static class Part2
+{
+    public static int Count(string[] input)
+    {
+        var width = input.First().Length;
+        var height = input.Length;
+
+        var possibleStartCoordinates = new List<(int x, int y)>();
+
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (input[y][x] is 'A' or 'a')
+                {
+                    possibleStartCoordinates.Add((x, y));
+                }
+            }
+        }
+
+        var xmasCount = possibleStartCoordinates
+            .Count(coords => IsValidCoordinates(coords.x, coords.y));
+
+        return xmasCount;
+
+        bool IsValidCoordinates(int x, int y)
+        {
+            if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+            {
+                return false;
+            }
+
+            string[] nwse = [
+                new([input[y-1][x-1],input[y][x],input[y+1][x+1]]),
+                new([input[y+1][x+1],input[y][x],input[y-1][x-1]]),
+            ];
+
+            string[] nesw = [
+                new([input[y+1][x-1],input[y][x],input[y-1][x+1]]),
+                new([input[y-1][x+1],input[y][x],input[y+1][x-1]]),
+            ];
+
+            return nwse.Any(word => string.Equals("mas", word, StringComparison.OrdinalIgnoreCase))
+                && nesw.Any(word => string.Equals("mas", word, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
