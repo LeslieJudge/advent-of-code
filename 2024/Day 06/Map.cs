19fa13582d6fbda75d888587ee288ac19a0bd325
Biró László <birolaszlo@hotmail.com>
2024-12-14 01:40:36 +0100

    Day 06 Part 2 works

--- 2024/Day 06/Map.cs
+++ 2024/Day 06/Map.cs
@@ -1,22 +1,30 @@
-﻿namespace Day_06;
+﻿using System.Diagnostics;
+
+namespace Day_06;
 
 internal sealed class Map
 {
     private readonly char[] _cells;
-    private readonly int _width;
-    private readonly int _height;
+    private char[] _backup;
     private char _guard;
     private int _guardX;
     private int _guardY;
     private int _directionX;
     private int _directionY;
+    
+    public int Width { get; }
+    public int Height { get; }
+    public bool ShowProgress { get; set; }
 
     private Map(char[] cells, int width, int height)
     {
-        (_cells, _width, _height) = (cells, width, width);
+        (_cells, Width, Height) = (cells, width, height);
         (_guardX, _guardY) = FindGuard();
         (_directionX, _directionY) = (0, -1);
         _guard = '^';
+
+        _backup = new char[Width * Height];
+        _cells.CopyTo(_backup, 0);
     }
 
     public static Map Load(string path)
@@ -35,19 +43,63 @@ internal sealed class Map
         return new Map(cells, width, lines.Length);
     }
 
-    public bool Step()
+    public void Reset()
     {
-        MarkAsVisited(_guardX, _guardY);
+        _backup.CopyTo(_cells, 0);
+        (_guardX, _guardY) = FindGuard();
+        (_directionX, _directionY) = (0, -1);
+        _guard = '^';
+    }
 
+    public bool FindLoop()
+    {
+        StepResult stepResult;
+
+        do
+        {
+            stepResult = Step();
+        } while (stepResult is StepResult.OK);
+
+        return stepResult is StepResult.LoopFound;
+    }
+
+    private bool NextStepWouldBeATurn(int x, int y)
+    {
+        x += _directionX;
+        y += _directionY;
+
+        return IsOccupied(x, y);
+    }
+
+    public StepResult Step()
+    {
         var x = _guardX + _directionX;
         var y = _guardY + _directionY;
+        var marker = 'X';
 
-        if (IsOccupied(x, y))
+        var turnCount = 0;
+
+        while (IsOccupied(x, y))
         {
             Turn();
+            turnCount++;
 
             x = _guardX + _directionX;
             y = _guardY + _directionY;
+
+            marker = 'x';
+        }
+
+        if (IsTurningPosition(x, y) && NextStepWouldBeATurn(x, y))
+        {
+            return StepResult.LoopFound;
+        }
+
+        MarkAsVisited(_guardX, _guardY, marker);
+
+        if (ShowProgress)
+        {
+            DrawCell(_guardX, _guardY, withColor: true);
         }
 
         _guardX = x;
@@ -55,12 +107,17 @@ internal sealed class Map
 
         if (GuardLeft())
         {
-            return false;
+            return StepResult.GuardLeft;
         }
 
-        _cells[_guardY * _width + _guardX] = _guard;
+        _cells[_guardY * Width + _guardX] = _guard;
 
-        return true;
+        if (ShowProgress)
+        {
+            DrawCell(_guardX, _guardY, withColor: true);
+        }
+
+        return StepResult.OK;
     }
 
     private void Turn()
@@ -95,52 +152,111 @@ internal sealed class Map
     private (int x, int y) FindGuard()
     {
         var index = _cells.AsSpan().IndexOf('^');
-        var y = index / _width;
-        var x = index % _width;
+        var y = index / Width;
+        var x = index % Width;
 
         return (x, y);
     }
 
-    private bool IsOccupied(int x, int y)
+    public void PlaceObstacle(int x, int y)
     {
-        if (x >= _width || y >= _height)
+        _cells[y * Width + x] = 'O';
+
+        if (ShowProgress)
+        {
+            DrawCell(x, y, withColor: true);
+        }
+    }
+
+    public bool IsOccupied(int x, int y)
+    {
+        if (IsOutOfBounds(x, y))
         {
             return false;
         }
 
-        return _cells[y * _width + x] is '#';
+        return _cells[y * Width + x] is '#' or 'O' or '^' or '>' or 'v' or '<';
     }
 
-    private void MarkAsVisited(int x, int y) => _cells[y * _width + x] = 'X';
+    private void MarkAsVisited(int x, int y, char mark) => _cells[y * Width + x] = mark;
 
-    private bool GuardLeft() => _guardX >= _width || _guardY >= _height;
-
-    public void PrintToConsole()
+    private bool IsTurningPosition(int x, int y)
     {
-        Console.CursorTop = 0;
-        Console.CursorLeft = 0;
-
-        for (var y = 0; y < _height; y++)
+        if (IsOutOfBounds(x, y))
         {
-            for (var x = 0; x < _width; x++)
+            return false;
+        }
+
+        return _cells[y * Width + x] is 'x';
+    }
+
+    private bool IsOutOfBounds(int x, int y) => x < 0 || Width <= x || y < 0 || Height <= y;
+
+    private bool GuardLeft() => IsOutOfBounds(_guardX, _guardY);
+
+    private char Cell(int x, int y) => _cells[y * Width + x];
+
+    public void Display(bool withColors)
+    {
+        Console.SetCursorPosition(0, 0);
+
+        if (withColors)
+        {
+            for (var y = 0; y < Height; y++)
             {
-                Console.ForegroundColor = _cells[y * _width + x] switch
+                for (var x = 0; x < Width; x++)
                 {
-                    '^' or '>' or 'v' or '<' => ConsoleColor.Yellow,
-                    '#' => ConsoleColor.Magenta,
-                    'X' => ConsoleColor.DarkCyan,
-                    _ => ConsoleColor.Gray,
-                };
+                    DrawCell(x, y, withColors);
+                }
 
-                Console.Write(_cells[y * _width + x]);
+                if (Console.CursorLeft != 0)
+                {
+                    Console.WriteLine();
+                }
             }
 
-            if (Console.CursorLeft != 0)
-            {
-                Console.WriteLine();
-            }
+            Console.ResetColor();
+        }
+        else
+        {
+            var map = string.Join("\r\n", _cells.Chunk(Width).Select(x => new string(x)));
+                
+            Console.ResetColor();
+            Console.WriteLine(map);
         }
     }
 
-    public int GetVisitedPositionCount() => _cells.Count(x => x is 'X');
+    public int GetVisitedPositionCount() => _cells.Count(x => x is 'X' or 'x');
+
+    private void SetColor(char cell)
+    {
+        Console.ForegroundColor = cell switch
+        {
+            '^' or '>' or 'v' or '<' => ConsoleColor.Yellow,
+            '#' => ConsoleColor.Magenta,
+            'O' => ConsoleColor.Green,
+            'X' or 'x' => ConsoleColor.DarkCyan,
+            _ => ConsoleColor.Gray,
+        };
+    }
+    private void DrawCell(int x, int y, bool withColor) => DrawCell(x, y, Cell(x, y), withColor);
+
+    private void DrawCell(int x, int y, char cell, bool withColor)
+    {
+        if (withColor)
+        {
+            SetColor(cell);
+        }
+
+        Console.SetCursorPosition(x, y);
+        Console.Write(cell);
+        Console.ResetColor();
+    }
+
+    public enum StepResult
+    {
+        OK,
+        GuardLeft,
+        LoopFound
+    }
 }
--- 2024/Day 06/Program.cs
+++ 2024/Day 06/Program.cs
@@ -4,14 +4,16 @@ var map = Map.Load("input.txt");
 var guardIsInTheArea = true;
 var autoStep = false;
 
+Console.SetBufferSize(Math.Max(Console.BufferWidth, map.Width), Math.Max(Console.BufferHeight, map.Height));
 Console.Clear();
+
+map.Display(withColors: true);
+map.ShowProgress = true;
 
 while (guardIsInTheArea)
 {
     if (!autoStep)
     {
-        map.PrintToConsole();
-
         var input = Console.ReadKey(intercept: true);
 
         if (input.Key is ConsoleKey.A)
@@ -20,11 +22,62 @@ while (guardIsInTheArea)
         }
     }
 
-    guardIsInTheArea = map.Step();
+    guardIsInTheArea = map.Step() is not Map.StepResult.GuardLeft;
 }
 
-map.PrintToConsole();
+map.Display(withColors: true);
 
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine($"Visited positions: {map.GetVisitedPositionCount()}");
+Console.ReadKey(intercept: true);
+Console.Clear();
+
+map.ShowProgress = false;
+var triedPositions = 0;
+var loopCount = 0;
+
+if (!map.ShowProgress)
+{
+    Console.WriteLine("Finding loops...");
+}
+
+for (var y = 0; y < map.Height; y++)
+{
+    for (var x = 0; x < map.Width; x++)
+    {
+        triedPositions++;
+        map.Reset();
+
+        if (map.IsOccupied(x, y))
+        {
+            continue;
+        }
+
+        if (map.ShowProgress)
+        {
+            map.Display(withColors: false);
+        }
+
+        map.PlaceObstacle(x, y);
+
+        if (map.FindLoop())
+        {
+            loopCount++;
+        }
+
+        if (map.ShowProgress)
+        {
+            Console.SetCursorPosition(0, map.Height + 2);
+            Console.WriteLine($"Tried obstacle positions: {triedPositions}");
+            Console.WriteLine($"Loops found: {loopCount}");
+        }
+    }
+}
+
+if (!map.ShowProgress)
+{
+    Console.WriteLine($"Tried obstacle positions: {triedPositions}");
+    Console.WriteLine($"Loops found: {loopCount}");
+}
+
